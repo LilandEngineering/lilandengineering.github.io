63bfabf6b09e140b8135dabebb1a3dbc0294bacf
LilandEngineering <LilandEngineering@users.noreply.github.com>
2020-04-16 09:50:46 +0200

    patch1.5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,20 +1,21 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "fecb086a097439b6452c239126367602",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "51d23d1c30deda6f34673e0d5600fd38",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "d51b09f7b8345b41dd3b2201f653c62b",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "0ea892e09437fcaa050b2b15c53173b7",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "63127007db837da47c24bb09b8d62ec1",
-"/main.dart.js": "43bf1c396db236dbc79698e2cf404d02",
-"/manifest.json": "68c2ce78e1f0d3e9882e56418a42ec1d"
+  "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "b74910f3b02aaab728b635ebe6092261",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "63127007db837da47c24bb09b8d62ec1",
+"/": "63127007db837da47c24bb09b8d62ec1",
+"main.dart.js": "aa36512d3edb3557d603071f221c5bda",
+"manifest.json": "68c2ce78e1f0d3e9882e56418a42ec1d"
 };
 
 self.addEventListener('activate', function (event) {
