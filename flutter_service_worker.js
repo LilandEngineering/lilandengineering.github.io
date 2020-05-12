45a55c883b0e1dca734a5ee027fe726a195aac85
LilandEngineering <LilandEngineering@users.noreply.github.com>
2020-05-12 08:57:12 +0200

    patch1.7
    Oppdatert UI for web og forenklet redigering for admin

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,10 +1,12 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
+  "assets/AssetManifest.json": "f8b3774432a412a5a4cc34ef026edac9",
+"assets/assets/Didaktisk_diamant.png": "828cf3d6344aee4a23ffd1a3401de609",
+"assets/assets/Didaktisk_diamant_rett.png": "318581bd81efd28a2bbb03a4bb62fe72",
 "assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "d37dc4c05802a87b543f618e946e0be0",
+"assets/LICENSE": "5444dcf844892dd1f6ded00ef70d9617",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
@@ -14,7 +16,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "63127007db837da47c24bb09b8d62ec1",
 "/": "63127007db837da47c24bb09b8d62ec1",
-"main.dart.js": "f0d6ddff869ac3b28515dc836a939f20",
+"main.dart.js": "f0d9254822ca9063fb0b225aa38ecb7a",
 "manifest.json": "68c2ce78e1f0d3e9882e56418a42ec1d"
 };
 
